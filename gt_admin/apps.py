0ffdc77e496d9875dbff04aea11e4add38a2d781
Danny-Yxzl <danny070601@aliyun.com>
2022-03-23 20:48:39 +0800

    [UPDATE] Update verbose_name in apps.

--- gt_admin/apps.py
+++ gt_admin/apps.py
@@ -1,6 +1,8 @@
+from tabnanny import verbose
 from django.apps import AppConfig
 
 
 class GtAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gt_admin'
+    verbose_name = '管理'
--- gt_article/apps.py
+++ gt_article/apps.py
@@ -1,6 +1,8 @@
+from tabnanny import verbose
 from django.apps import AppConfig
 
 
 class GtArticleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gt_article'
+    verbose_name = '文章'

fd3208e1e8907c72f554bca5266ee815ac4ef746
Danny-Yxzl <danny070601@aliyun.com>
2022-04-01 14:41:24 +0800

    [DELETE] Disabled API test pages.

--- gt/settings.py
+++ gt/settings.py
@@ -165,8 +165,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':
+    20,
+    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer']
 }
 
 AUTH_USER_MODEL = 'gt_user.User'

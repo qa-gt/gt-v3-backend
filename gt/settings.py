5fd4078126c9ef2d7ab519706f0c3578e5900ee0
Danny-Yxzl <danny070601@aliyun.com>
2022-04-02 11:14:32 +0800

    [ADD] Add DEFAULT_THROTTLE

--- gt/settings.py
+++ gt/settings.py
@@ -189,7 +189,15 @@ REST_FRAMEWORK = {
     'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':
     20,
-    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer']
+    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/min',
+        'user': '50/min'
+    }
 }
 
 AUTH_USER_MODEL = 'gt_user.User'

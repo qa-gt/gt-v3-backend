ed206310ce811e36e7149b1756da74979a74e8df
Danny-Yxzl <danny070601@aliyun.com>
2022-05-14 12:11:07 +0800

    [UPDATE] Finish Tape

--- gt_tape/views.py
+++ gt_tape/views.py
@@ -4,42 +4,70 @@ from .models import *
 from .serializers import *
 from .filters import QuestionFilter
 from .permissions import TapeBoxPermission
-from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from gt.permissions import *
 from gt_notice.options import add_notice
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
-from rest_framework.permissions import *
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, mixins
 
 
 def random_string(length=10):
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+    return ''.join(
+        random.choice(string.ascii_letters + string.digits)
+        for _ in range(length))
 
 
-class TapeBoxViewSet(mixins.RetrieveModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, GenericViewSet):
+class TapeBoxViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+                     GenericViewSet):
     queryset = TapeBox.objects.all()
     serializer_class = TapeBoxSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, TapeBoxPermission)
+
+    def create(self, request, *args, **kwargs):
+        if TapeBox.objects.filter(user=request.user).exists():
+            raise ValidationError('您的TapeBox已经存在')
+        tape_box = TapeBox(user=request.user,
+                           title=request.data['title'],
+                           image=request.data['image'])
+        tape_box.save()
+        return Response(TapeBoxSerializer(tape_box).data)
+
+    @action(detail=False,
+            methods=['GET'],
+            permission_classes=[IsAuthenticated],
+            url_path='me')
+    def get_my_tape_box(self, request):
+        tape_box = TapeBox.objects.filter(user=request.user)
+        if tape_box.exists():
+            return Response(TapeBoxSerializer(tape_box.first()).data)
+        return Response({'not_found': True})
 
 
 class TapeQuestionViewSet(mixins.ListModelMixin, GenericViewSet):
     queryset = TapeQuestion.objects.all().order_by('-time')
     serializer_class = TapeQuestionSerializer
     permission_classes = [RobotCheck, NoEdit]
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, )
     filterset_class = QuestionFilter
 
     @staticmethod
     def create(request, *args, **kwargs):
         try:
-            question = TapeQuestion(box=TapeBox.objects.get(id=request.data['box']), content=request.data['content'],
-                                    author=request.user, token=random_string())
+            tape_box = TapeBox.objects.get(id=request.data['box'])
+            question = TapeQuestion(box=tape_box,
+                                    content=request.data['content'],
+                                    author=request.user,
+                                    token=random_string())
             question.save()
+            add_notice(tape_box.user, "您的TapeBox有新的问题",
+                       request.data['content'],
+                       "/tapebox/" + request.data['box'])
+            return Response(TapeQuestionSerializer(question).data, 201)
         except TapeBox.DoesNotExist:
             raise ValidationError('问题创建失败')
-        return Response(TapeQuestionCreatingSerializer(question).data, status=201)
 
 
 class TapeReplyViewSet(GenericViewSet):
@@ -50,11 +78,17 @@ class TapeReplyViewSet(GenericViewSet):
     def create(request, *args, **kwargs):
         try:
             question = TapeQuestion.objects.get(id=request.data['question'])
-            if request.data['token'] != question.token and (not request.user or request.user != question.author):
+            if request.data['token'] != question.token and (
+                    not request.user or request.user != question.author):
                 raise AuthenticationFailed('提问者身份验证失败')
-            reply = TapeReply(question=question, content=request.data['content'],
+            reply = TapeReply(question=question,
+                              content=request.data['content'],
                               is_owner=request.user == question.box.user)
             reply.save()
+            if reply.is_owner and question.author:
+                add_notice(question.author, "您的Tape提问有新的回复",
+                           request.data['content'],
+                           "/tapebox/" + str(question.id))
+            return Response(status=201)
         except TapeQuestion.DoesNotExist:
             raise ValidationError('回复失败')
-        return Response(status=201)
